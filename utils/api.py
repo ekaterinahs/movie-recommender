48d13283ea8de3db744e7990b64114ce7bb16f80
Ekaterina Hristova Stankulova <87805119+ekaterinahs@users.noreply.github.com>
2025-02-25 14:21:51 +0000

    Changes in API handling: movie searching is ordered now by vote count (appearing movies with more votes first).

--- utils/api.py
+++ utils/api.py
@@ -14,17 +14,19 @@ def get_top5_movies(movie_name):
     Fetches multiple movies matching the query.
     """
     url = f"{BASE_URL}search/movie?api_key={API_KEY}&query={movie_name}&page=1"
-    response = requests.get(url).json()
+    response_aux = requests.get(url).json()
 
-    if "results" not in response or len(response["results"]) == 0:
+    if "results" not in response_aux or len(response_aux["results"]) == 0:
         return None # No movies found
+    
+    response = sorted(response_aux["results"], key=lambda d: d['vote_count'], reverse=True)
 
     movies = []
-    for movie in response["results"][:5]: # Get top 5 results
+    for movie in response[:5]: # Get top 5 results
         movies.append({
             "id": movie["id"],
             "title": movie["title"],
-            "year": movie.get("release_date", "Unknown")[:4],
+            "year": movie.get("release_date", "Unknown")[:4] if movie.get("release_date") else "Unknown",
             "poster": f"https://image.tmdb.org/t/p/w200{movie.get('poster_path')}" if movie.get("poster_path") else None
         })
     return movies
@@ -44,8 +46,8 @@ def get_collaborative_recommendations(movie_id, num_results=5):
         recommendations.append({
             "id": movie["id"],
             "title": movie["title"],
-            "year": movie.get("release_date", "Unknown")[:4],
-            "description": movie.get("overview", "No description available."),
+            "year": movie.get("release_date", "Unknown")[:4] if movie.get("release_date") else "Unknown",
+            "description": movie.get("overview", "No description available.") if movie.get("overview") else "No description available.",
             "poster": f"https://image.tmdb.org/t/p/w200{movie.get('poster_path')}" if movie.get("poster_path") else None
         })
     return recommendations
@@ -65,8 +67,8 @@ def get_content_based_recommendations(movie_id, num_results=5):
         recommendations.append({
             "id": movie["id"],
             "title": movie["title"],
-            "year": movie.get("release_date", "Unknown")[:4],
-            "description": movie.get("overview", "No description available."),
+            "year": movie.get("release_date", "Unknown")[:4] if movie.get("release_date") else "Unknown",
+            "description": movie.get("overview", "No description available.") if movie.get("overview") else "No description available.",
             "poster": f"https://image.tmdb.org/t/p/w200{movie.get('poster_path')}" if movie.get("poster_path") else None
         })
     return recommendations
